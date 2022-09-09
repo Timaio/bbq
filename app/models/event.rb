1e147893e59c0746fd838fb1a661dd3f9237e12e
Timaio <timeiqu@gmail.com>
2022-09-09 11:13:29 +0300

    Deleting events fixed

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,7 @@ class Event < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :subscriptions, dependent: :destroy
   has_many :subscribers, through: :subscriptions, source: :user
-  has_many :photos, class_name: "Photo"
+  has_many :photos, dependent: :delete_all
   
   validates :user, presence: true
   validates :title, presence: true, length: { maximum: 255 }
