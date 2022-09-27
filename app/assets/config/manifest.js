1bb41152edd705796c0d3ccaeb1ae94525f08f4d
Timaio <timeiqu@gmail.com>
2022-09-27 05:05:31 +0300

    Lightbox

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,10 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-//= link_tree ../../javascript .js
+//= link_tree ../javascripts .js
+//= link_tree ../../../vendor/javascript .js
+//= link_tree ../../../vendor/stylesheets .css
+//= link_tree ../../../vendor/assets/lightbox2-2.11.3
+
+//= link lightbox.min.css
+//= link lightbox-plus-jquery.min.js
 //= link map.js
