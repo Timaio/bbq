e4f9690fe7f1e118680df5a9b678b30be5016d73
Timaio <timeiqu@gmail.com>
2022-09-30 00:44:27 +0300

    Turbo fix, templates fix

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,7 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_tree ../javascripts .js
+//= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= link_tree ../../../vendor/stylesheets .css
 //= link_tree ../../../vendor/assets/lightbox2-2.11.3
