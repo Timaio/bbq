8934f34a53b79fdd4c5a047eda16cc011d7792ae
Timaio <timeiqu@gmail.com>
2022-09-30 15:19:15 +0300

    ActiveStorage fix, Map fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,8 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :yandex
+  config.active_storage.variant_processor = :mini_magick
 
   config.action_mailer.perform_caching = false
 
